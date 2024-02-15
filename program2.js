6e0eb7269ff23d4aac656532833a52668597eb29
Prachikarma09 <prachikarma09@gmail.com>
2024-02-15 15:14:18 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -9,7 +9,7 @@ public class romanToInt {
         romanMap.put('V', 5);
         romanMap.put('X', 10);
         romanMap.put('L', 50);
-        romanMap.put('', 1);
+        romanMap.put('C', 1);
 
 
 
