d2c83d862af68e401a8e1597d35316486315230f
Prachikarma09 <prachikarma09@gmail.com>
2024-02-15 15:22:08 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -2,7 +2,7 @@
  * @param {string} s
  * @return {number}
  */
-var class romanToInt {
+var romanToInt {
     public int romanToInt(String s){
         Map<CharacterData, Integer> romanMap = new Hashmap<>();
         romanMap.put('I', 1);
