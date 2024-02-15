f95c741dd94b88ae21b6a58dc5a6606087b66e76
Prachikarma09 <prachikarma09@gmail.com>
2024-02-15 15:23:27 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -5,7 +5,7 @@
 var romanToInt =function(s){
     const romanMap= new Map();
         Map<CharacterData, Integer> romanMap = new Hashmap<>();
-        romanMap.put('I', 1);
+        romanMap.sett('I', 1);
         romanMap.put('V', 5);
         romanMap.put('X', 10);
         romanMap.put('L', 50);
